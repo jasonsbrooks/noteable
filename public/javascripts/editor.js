486584c668971e65fe315c77f88ea65fcdfdb51e
Minh Tri Pham <minhtri@pham.cz>
2015-02-13 01:27:10 -0500

    buttons

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -3,10 +3,43 @@ $(document).ready(function() {
 		beforeSubmit: function(formData, formObject, formOptions) {
 			formData.push(
 				{name: 'contents', value: editor.getHTML()}
-			);
+				);
 		},
 		success: function(html, status, xhr, myForm) {
 			console.log("successful");
 		}
 	})
+
+	$('#tokenfield').tokenfield({
+		autocomplete: {
+			delay: 100
+		},
+		showAutocompleteOnFocus: true
+	});
+
+	$("#shareShareButton").click(function() {
+		$("#shareModal").modal("hide");
+
+	});
+
+	$('#shareModal').on('hidden.bs.modal', function () {
+		$(".token").remove();
+	});
+
+
+
+	$.fn.modal.Constructor.prototype.enforceFocus = function () {
+		var that = this;
+		$(document).on('focusin.modal', function (e) {
+			if ($(e.target).hasClass('select2-input')) {
+				return true;
+			}
+
+			if (that.$element[0] !== e.target && !that.$element.has(e.target).length) {
+				that.$element.focus();
+			}
+		});
+	};
+
 });
+
